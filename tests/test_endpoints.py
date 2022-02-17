3f8a0e6575c9aeb058b1c334d86dd81edbf39642
christianwaldmann <waldmann-christian@web.de>
2022-02-17 01:36:48 +0100

    Add basic tests to endpoints

---
+++ tests/test_endpoints.py
@@ -0,0 +1,36 @@
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_joke_random(client):
+    res = client.get("/joke")
+    assert res.status_code == 200
+    assert "id" in res.json.keys()
+    assert "value" in res.json.keys()
+
+
+def test_joke_random_reverse(client):
+    res = client.get("/joke")
+    assert res.status_code == 200
+    assert "id" in res.json.keys()
+    assert "value" in res.json.keys()
+
+
+def test_joke_id(client):
+    res = client.get("/joke/a7G4M0EuRXiGa79XllTcKA")
+    assert res.status_code == 200
+    assert res.json["id"] == "a7G4M0EuRXiGa79XllTcKA"
+    assert res.json["value"] == "Chuck Norris can beat anybody in a staring contest... with his eyes closed"
+
+
+def test_joke_id_reverse(client):
+    res = client.get("/joke/a7G4M0EuRXiGa79XllTcKA/reverse")
+    assert res.status_code == 200
+    assert res.json["id"] == "a7G4M0EuRXiGa79XllTcKA"
+    assert res.json["value"] == "kcuhC sirroN nac taeb ydobyna ni a gnirats tsetnoc... htiw sih seye desolc"
+
