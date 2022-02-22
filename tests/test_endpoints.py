d4b64c92ef99112c78b66d5e707abb2645308ed0
christianwaldmann <waldmann-christian@web.de>
2022-02-22 20:13:10 +0100

    Add partial tests for author routes

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from app import create_app, setup_database
+import json
 
 
 app = create_app()
@@ -40,3 +41,27 @@ def test_joke_id_reverse(client):
     assert res.json["id"] == "a7G4M0EuRXiGa79XllTcKA"
     assert res.json["value"] == "kcuhC sirroN nac taeb ydobyna ni a gnirats tsetnoc... htiw sih seye desolc"
 
+
+# TODO:
+# - add tests for update_author, assign_author_to_joke and get_author
+# - rewrite this test so I don't test on the actual db (maybe use pytest-postgresql for that)
+def test_add_author_and_delete_author(client):
+    res = client.post(
+        "/author",
+        data=json.dumps({"firstname": "Bob", "lastname": "Fischer"}),
+        headers={"Content-Type": "application/json"}
+    )
+    assert res.status_code == 201
+    assert res.json["firstname"] == "Bob"
+    assert res.json["lastname"] == "Fischer"
+
+    author_id = res.json["id"]
+    res = client.delete(f"/author/{author_id}")
+    assert res.status_code == 200
+    assert res.json["firstname"] == "Bob"
+    assert res.json["lastname"] == "Fischer"
+
+
+def test_get_all_authors(client):
+    res = client.get("/author")
+    assert res.status_code == 200
